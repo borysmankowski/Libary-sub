0a9ba101bcdc96cde24bff421a28ba872d472161
bm <borys.mankowski@gmail.com>
2024-01-11 17:38:31 +0100

    changes v27
    1 test failing

--- src/main/java/com/example/borys_mankowski_test_10/email/EmailSchedule.java
+++ src/main/java/com/example/borys_mankowski_test_10/email/EmailSchedule.java
@@ -4,8 +4,6 @@ import com.example.borys_mankowski_test_10.appuser.AppUserRepository;
 import com.example.borys_mankowski_test_10.appuser.model.AppUser;
 import com.example.borys_mankowski_test_10.book.BookRepository;
 import com.example.borys_mankowski_test_10.book.model.Book;
-import com.example.borys_mankowski_test_10.subscription.SubscriptionRepository;
-import com.example.borys_mankowski_test_10.subscription.model.Subscription;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,7 +11,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -24,54 +23,32 @@ public class EmailSchedule {
 
     private final BookRepository bookRepository;
 
-    private final SubscriptionRepository subscriptionRepository;
-
     private final AppUserRepository appUserRepository;
 
     @Scheduled(cron = "${scheduled.email.notification.cron}")
     public void sendScheduledEmailNotification() {
-        LocalDate todayDateTime = LocalDate.now();
-        int appUserBatchSize = 1000;
-        int bookBatchSize = 1000;
-
-        int appUserPageNumber = 0;
-        int bookPageNumber;
-
-        Page<AppUser> appUsers;
+        Pageable usersPageRequest = PageRequest.of(0, 1000);
+        Page<AppUser> pageOfUsersToNotify;
         do {
-            Pageable appUserPageable = PageRequest.of(appUserPageNumber, appUserBatchSize);
-            appUsers = appUserRepository.findUsersSubscribedForBooksAddedToday(todayDateTime, appUserPageable);
-
-            for (AppUser appUser : appUsers) {
-                bookPageNumber = 0;
-                Pageable bookPageable = PageRequest.of(bookPageNumber, bookBatchSize);
-
-                Page<Subscription> subscriptions = subscriptionRepository.findAllByAppUser_Email(appUser.getEmail(), appUserPageable);
-
-                for (Subscription subscription : subscriptions) {
-                    Page<Book> newBooks = bookRepository.findAllByAddedDateAndCategoryOrAuthor(
-                            todayDateTime,
-                            subscription.getBookCategory(),
-                            subscription.getBookAuthor(),
-                            bookPageable
-                    );
-
-                    while (newBooks.hasContent()) {
-                        emailService.sendNotificationIfNewBooks(appUser.getEmail(), newBooks.getContent());
-                        bookPageNumber++;
-                        bookPageable = PageRequest.of(bookPageNumber, bookBatchSize);
-                        newBooks = bookRepository.findAllByAddedDateAndCategoryOrAuthor(
-                                todayDateTime,
-                                subscription.getBookCategory(),
-                                subscription.getBookAuthor(),
-                                bookPageable
-                        );
-                    }
-                }
+            pageOfUsersToNotify = appUserRepository.findUsersSubscribedForBooksAddedToday(usersPageRequest);
+            for (AppUser userToNotify : pageOfUsersToNotify) {
+                notifyUser(userToNotify);
             }
-            appUserPageNumber++;
+            usersPageRequest = usersPageRequest.next();
+        } while (pageOfUsersToNotify.hasNext());
+    }
 
-        } while (!appUsers.isEmpty());
+    private void notifyUser(AppUser userToNotify) {
+        Pageable booksPageRequest = PageRequest.of(0, 1000);
+        Page<Book> pageOfBooks;
+        List<Book> booksToSendInEmail = new ArrayList<>();
+        do {
+            pageOfBooks = bookRepository.findAllAddedTodayMatchingUserSubscriptions(userToNotify.getId(), booksPageRequest);
+            booksToSendInEmail.addAll(pageOfBooks.getContent());
+            booksPageRequest = booksPageRequest.next();
+        } while (pageOfBooks.hasNext());
+
+        emailService.sendNotificationIfNewBooks(userToNotify.getEmail(), booksToSendInEmail);
     }
 }
 
